da7291754e15b2af7799d064fb6ec44da27ef016
Vinicius Felix <viniciusfelix.exe@gmail.com>
2020-05-19 16:13:37 -0300

    Changed API to Callbacks and Receptivos

--- src/app/controllers/callbacks.js
+++ src/app/controllers/callbacks.js
@@ -2,6 +2,7 @@ const express = require('express');
 const Callbacks = require('../models/Callbacks');
 const router = express.Router();
 
+// Exibir todos os registros
 router.get('/', async(req, res) => {
   try{
     const callbacks = await Callbacks.find();
@@ -11,12 +12,33 @@ router.get('/', async(req, res) => {
   }
 });
 
+// Cadastrar novo callback
 router.post('/registrar', async(req, res) => {
   try {    
     const callbacks = await Callbacks.create(req.body);
     return res.send({ callbacks });
   } catch(err) {
     return res.status(400).send({ error: 'Erro ao registrar novo callback!'});
+  }
+});
+
+// Atualizar registro existente
+router.put('/atualiza/:callbackId', async (req, res) => {
+  try {
+    const {description} = req.body;
+    const callback = await Callbacks.findByIdAndUpdate(req.params.callbackId, { description });
+    return res.send({ callback });
+  } catch (err) {
+    return res.status(400).send({ error: 'Não foi possivel atualizar o callback! ' + err });
+  }
+});
+
+// Apagar registro existente
+router.delete('/apagar/:callbackId', async (req, res) => {
+  try {
+    const callback = await Callbacks.findByIdAndRemove(req.params.callbackId);
+  } catch (err) {
+    return res.status(400).send({ error: 'Não foi possivel apagar o callback! ' + err })
   }
 });
 
--- src/app/controllers/receptivos.js
+++ src/app/controllers/receptivos.js
@@ -2,6 +2,7 @@ const express = require('express');
 const Receptivos = require('../models/Receptivos');
 const router = express.Router();
 
+// Exibir todos os registros
 router.get('/', async(req, res) => {
   try{
     const receptivos = await Receptivos.find();
@@ -11,12 +12,33 @@ router.get('/', async(req, res) => {
   }
 });
 
+// Cadastrar novo receptivos
 router.post('/registrar', async(req, res) => {
   try {    
     const receptivos = await Receptivos.create(req.body);
     return res.send({ receptivos });
   } catch(err) {
-    return res.status(400).send({ error: 'Erro ao registrar novo telefone!'});
+    return res.status(400).send({ error: 'Erro ao registrar novo receptivo! ' + err});
+  }
+});
+
+// Atualizar registro existente
+router.put('/atualiza/:receptivoId', async (req, res) => {
+  try {
+    const {description} = req.body;
+    const receptivo = await Receptivos.findByIdAndUpdate(req.params.receptivoId, { description });
+    return res.send({ receptivo });
+  } catch (err) {
+    return res.status(400).send({ error: 'Não foi possivel atualizar o receptivo! ' + err });
+  }
+});
+
+// Apagar registro existente
+router.delete('/apagar/:receptivoId', async (req, res) => {
+  try {
+    const receptivo = await Receptivos.findByIdAndRemove(req.params.receptivoId);
+  } catch (err) {
+    return res.status(400).send({ error: 'Não foi possivel apagar o receptivo! ' + err })
   }
 });
 
