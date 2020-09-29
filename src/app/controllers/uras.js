a7f4762b0e7356ebc6f52ad016d1add0307ddb83
Vinicius Felix <viniciusfelix.exe@gmail.com>
2020-09-28 23:58:38 -0300

    Fixed bug on uras.js

--- src/app/controllers/uras.js
+++ src/app/controllers/uras.js
@@ -9,50 +9,47 @@ router.get('/', async(req, res) => {
 		(async () => {
 
 			console.log('Acessando URAs');
-		
+
 			let data, custo = [];
 
 			const browser = await puppeteer.launch({ args: [ '--ignore-certificate-errors', '--disable-dev-shm-usage' ] });
 
-				let page = await browser.newPage();
+			let page = await browser.newPage();
 
-				await page.goto('http://192.168.170.21:8080/rj/login.html', { timeout: 0 });
+			await page.goto('http://192.168.170.21:8080/rj/login.html', { timeout: 0 });
+	
+			await page.type('#username', process.env.JSI_URAS_USERNAME);
+			await page.type('#password', process.env.JSI_URAS_PASSWORD);
 		
-				await page.type('#username', process.env.JSI_URAS_USERNAME);
-				await page.type('#password', process.env.JSI_URAS_PASSWORD);
+			await page.click('#loginButton');
+	
+			await page.waitForNavigation();
 			
-				await page.click('#loginButton');
-		
-				await page.waitForNavigation();
-				
-				await page.goto('http://192.168.170.21:8080/rj/custos-ura.html');
+			await page.goto('http://192.168.170.21:8080/rj/custos-ura.html');
 			
 			for(let i = 7; i < 21; i++){
-				
+
 				const selector_i = 'i[class="ft-plus"]';
 				await page.evaluate((selector_i) => document.querySelector(selector_i).click(), selector_i); 
-		
+
 				await page.type('#horas', i.toString());
-		
+
 				const selector = 'button[class="btn btn-primary"]';
 				await page.evaluate((selector) => document.querySelector(selector).click(), selector); 
-		
+
 				await page.waitForSelector('div[class="card-content"]');
-				
+
 				data = await page.evaluate(() => {
-					return document.querySelector('div[class="card-content"]').innerText;
+					return document.querySelector('div[class="col-md-4 col-sm-12"]').innerText
+					.split('\n')[0]
+					.split('R$')[1]
+					.trim();
 				});
-		
-				data = data.split('\n');
-		
-				if(data.length >= 15){
-					custo.push(data[12].split('R$').join('').trim());
-				} else {
-					custo.push('0,00');
-				}//if-else
+
+				custo.push(data);
 
 				await page.evaluate(() => ( document.querySelector('input[name="horas"]').value = '' ));
-				
+
 			}//for
 
 			await page.close();
